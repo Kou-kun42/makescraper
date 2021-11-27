74563a233b86ea54ab68367b07788fe5ee340014
Kou-kun42 <shinigamithekido@gmail.com>
2021-11-26 19:35:12 -0500

    testing selectors out

--- scrape.go
+++ scrape.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
-
+	//"io/ioutil"
+	//"strings"
+	//"encoding/json"
 	"github.com/gocolly/colly"
 )
+
+
+type recipe struct {
+	Name string `json:"name"`
+	Ingredients []string `json:"ingredients"`
+	Directions []string `json:"directions"`
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -12,12 +21,16 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	//title := "h1.recipe-title"
+	ingredients_list := "ul.recipe-ingredients__list li"
+	directions_list := "ol.recipe-directions__list li span"
+	var ingredients []string
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// On every a element which has href attribute call callback
+	c.OnHTML(ingredients_list, func(e *colly.HTMLElement) {
+                ing := e.Text
+
+				ingredients = append(ingredients, ing)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +39,6 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.tasteofhome.com/recipes/rum-balls/")
+	fmt.Println(ingredients)
 }
