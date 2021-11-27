0c0eafe1595ab053e70a98a4f54ed6a88f5a98ae
Kou-kun42 <shinigamithekido@gmail.com>
2021-11-26 22:46:20 -0500

    Got base recipe scraper working

--- scrape.go
+++ scrape.go
@@ -9,8 +9,8 @@ import (
 )
 
 
-type recipe struct {
-	Name string `json:"name"`
+type Recipe struct {
+	Title string `json:"title"`
 	Ingredients []string `json:"ingredients"`
 	Directions []string `json:"directions"`
 }
@@ -21,24 +21,42 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	//title := "h1.recipe-title"
+	// Set up selectors and storage
+	title_sel := "h1.recipe-title"
 	ingredients_list := "ul.recipe-ingredients__list li"
 	directions_list := "ol.recipe-directions__list li span"
+	var title string
 	var ingredients []string
-
-	// On every a element which has href attribute call callback
-	c.OnHTML(ingredients_list, func(e *colly.HTMLElement) {
-                ing := e.Text
-
-				ingredients = append(ingredients, ing)
-	})
+	var directions []string
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	// Get the title
+	c.OnHTML(title_sel, func(e *colly.HTMLElement) {
+        title = e.Text
+	})
+
+	// Get the ingredients
+	c.OnHTML(ingredients_list, func(e *colly.HTMLElement) {
+		ingredients = append(ingredients, e.Text)
+	})
+
+	// Get the directions
+	c.OnHTML(directions_list, func(e *colly.HTMLElement) {
+		directions = append(directions, e.Text)
+	})
+
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://www.tasteofhome.com/recipes/rum-balls/")
-	fmt.Println(ingredients)
+	
+	// Save as recipe
+	recipe := Recipe{
+		Title: title,
+		Ingredients: ingredients,
+		Directions: directions,
+	}
+	fmt.Println(recipe)
 }
